c9306bfb816c73a68f38a19a1368727a3e123ffb
Rene Raidmaa <55079635+rein-ra@users.noreply.github.com>
2022-09-28 16:03:44 +0300

    Yes

---
+++ src/components/slider.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <div class="range-input">
+            <input type="range" class="range-min" min="0" max="1" value="0.5" step="0.05">
+            <input type="range" class="range-max" min="0" max="1" value="0.5" step="0.05">
+            
+        </div>
+    </header>
+</template>
+
+<script>
+
+    let rangeValue
+
+    function setEndPos(value){
+        alert(value)
+    }
+
+    /* slider.onchange() = function(){
+        rangeValue = slider.value
+    }*/
+
+
+
+
+    //console.log(document.getElementById('rangeSelector').value)
+
+    export default {
+        components:{
+            name: 'Header'
+        }
+    }
+
+</script>
